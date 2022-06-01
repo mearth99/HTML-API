d7d40723c9a5c8d6b375681ca4b99c7198ad3ad9
Lee jin ki <dlwlsrl99@gmail.com>
2022-06-02 01:53:57 +0900

    post and qurl GPT 2 long

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, request
+import requests
+
+url = "https://main-gpt2-large-jeong-hyun-su.endpoint.ainize.ai/gpt2-large/long"
 
 app = Flask(__name__)
 @app.route('/')
@@ -9,7 +12,22 @@ def index():
 def result():
      if request.method == 'POST':
         result = request.form
-        return render_template("result.html",result = result)
+        text = result["Name"]
+        data = {
+            "text": text, 
+            "num_samples": 5,
+            "length": 10
+        }
+        response = requests.post(url, data=data)
+
+# print res if the request is successful
+        if response.status_code == 200:
+            res = response.json()
+            print(res)
+        else:
+            print("Failed requests")
+        return render_template("result.html",result = res)
     
 if __name__ == '__main__':
     app.run(debug=True)
+
