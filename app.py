35d973ac0324efc568140bc623b6f271eadad8e4
Lee jin ki <dlwlsrl99@gmail.com>
2022-06-03 03:20:52 +0900

    connect answer and print web

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
+from unicodedata import category
 from flask import Flask, render_template, request
-import requests
+import requests, random, json
 
-url = "https://main-gpt2-large-jeong-hyun-su.endpoint.ainize.ai/gpt2-large/long"
+url = "https://master-gpt2-everytime-fpem123.endpoint.ainize.ai/everytime/fix-length"
 
 app = Flask(__name__)
 @app.route('/')
@@ -13,21 +14,30 @@ def result():
      if request.method == 'POST':
         result = request.form
         text = result["Name"]
+        category = result["Select"]
+        qurl_num = int(result["Number"])
         data = {
             "text": text, 
-            "num_samples": 5,
+            "category": category,
             "length": 10
         }
-        response = requests.post(url, data=data)
-
+        if(qurl_num>10):
+            qurl_num=10
+        sum = []
+        for i in range(0,qurl_num):
+            data["length"] = random.randrange(10,30)
+            response = requests.post(url, data=data)
 # print res if the request is successful
-        if response.status_code == 200:
-            res = response.json()
-            print(res)
-        else:
-            print("Failed requests")
-        return render_template("result.html",result = res)
-    
+            if response.status_code == 200:
+                res = response.json()
+                output = res["0"]
+                sum.append(str(i)+". " + output)
+            else:
+                print("Failed requests")
+        return render_template("result.html",result = sum)
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
 
